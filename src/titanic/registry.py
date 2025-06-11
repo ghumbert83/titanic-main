981e91c33b3fde347a6063f3564e768526caa710
Stephane Romany <stephrm972@gmail.com>
2025-06-11 14:25:06 +0000

    adding save/load modle with pickle

--- src/titanic/registry.py
+++ src/titanic/registry.py
@@ -2,25 +2,44 @@
 Save and load models, preprocessors
 """
 
+import pickle, os
 
-def save_model(model, path: str):
+def save_model(model, path: str, filename: str) -> None:
     """
     Save a model to the specified path.
-    
     Args:
-        model: The model to save.
-        path (str): The file path where the model will be saved.
+        model (object): The model to be saved.
+        path (str): The directory where the model will be saved.
+        filename (str): The name of the file to save the model as.
+    Returns:
+        None
     """
-    pass
 
-def load_model(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    with open(os.path.join(path, filename, "wb")) as f:
+        pickle.dump(model, f)
+
+
+def load_model(filepath: str) -> object:
     """
     Load a model from the specified path.
-    
+
     Args:
         path (str): The file path from which the model will be loaded.
-        
+
     Returns:
         The loaded model.
     """
-    pass
+    with open(filepath, "rb") as f:
+        model = pickle.load(f)
+
+    if not model:
+        raise ValueError(f"Model at {filepath} is empty or could not be loaded.")
+    return model
+
+
+
+if __name__ == "__main__":
+    print(pickle.dumps({"a": 1, "b": 2, "c": 3}))
