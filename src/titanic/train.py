6e2a7b19fbc7a2184b9d4214b1bb737114fd7e79
Stephane Romany <stephrm972@gmail.com>
2025-06-11 10:43:04 +0000

    implements train and evaluate function

--- src/titanic/train.py
+++ src/titanic/train.py
@@ -2,17 +2,29 @@
 Train the Titanic model.
 """
 
+from titanic.data import clean_data, prepare_data, load_data
+from sklearn.linear_model import LogisticRegression
 
 
-def train_model():
-    """ 
+def train_model() -> object:
+    """
     Initiate the model and train it on the Titanic dataset.""
     """
-    pass
 
-def evaluate_model():
-    pass
+    train_df, _ = load_data()
+    train_df_cleaned = clean_data(train_df)
+    X_train, y_train = prepare_data(train_df_cleaned)
+    model = LogisticRegression(max_iter=1000, random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
-def optimize_model():
-    pass
 
+def evaluate_model(model):
+    _, test_df = load_data()
+    test_cleaned_df = clean_data(test_df)
+    X_test, y_test = prepare_data(test_cleaned_df)
+    model.score(X_test, y_test)
+
+
+def optimize_model(model):
+    pass
